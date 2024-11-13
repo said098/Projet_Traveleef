791b41f50c93e2a89db3de6103c173427d23bbb6
saidkamal <saidkamalshinwari098@gmail.com>
2024-11-14 00:02:22 +0100

    ajouter component de header

--- frontend/projettraveleef/src/app/layout/header/header.component.ts
+++ frontend/projettraveleef/src/app/layout/header/header.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {ConnexionComponent} from '../../fonctionnalites/connexion/connexion.component';
 import {FormsModule} from '@angular/forms';
-import {EtudiantsComponent} from '../../fonctionnalites/etudiants/etudiants.component';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports: [RouterLink,ConnexionComponent, FormsModule, EtudiantsComponent, RouterLink,],
+  imports: [RouterLink, FormsModule,  RouterLink,],
   standalone: true,
 })
 export class HeaderComponent {
