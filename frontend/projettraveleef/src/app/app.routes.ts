7092610c42edcb9484f162474f536b7407f4c640
saidkamal <saidkamalshinwari098@gmail.com>
2024-11-25 10:48:23 +0100

    Commenté les routes inutilisées dans app.routes.ts.

--- frontend/projettraveleef/src/app/app.routes.ts
+++ frontend/projettraveleef/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
-  { path: 'users', component: UsersComponent }
+  //{ path: '', redirectTo: '/users', pathMatch: 'full' },
+  //{ path: 'users', component: UsersComponent }
 ];
