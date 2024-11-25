8157ca737ca3729594eab386f1a8af081306d429
Immannn <i.amimman0.0@gmail.com>
2024-11-25 15:07:15 +0100

    New composant , Voyage , user-info

--- frontend/projettraveleef/src/app/app.routes.ts
+++ frontend/projettraveleef/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { UserInfoComponent } from './users/user-info/user-info.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
-  { path: 'users', component: UsersComponent }
+  
+  { path: 'users', component: UsersComponent , 
+    children : 
+    [
+      {path: 'user-info', component : UserInfoComponent}
+    ]
+  }
 ];
---
+++ frontend/projettraveleef/src/app/voyage/voyage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voyage',
+  standalone: true,
+  imports: [],
+  templateUrl: './voyage.component.html',
+  styleUrl: './voyage.component.css'
+})
+export class VoyageComponent {
+
+}
