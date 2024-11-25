67eda6cecb9653417be98f3051f594c1c297fdf1
saidkamal <saidkamalshinwari098@gmail.com>
2024-11-25 10:48:17 +0100

    Mise à jour de app.component.ts : ajout des composants Footer et PageAccueil.

--- frontend/projettraveleef/src/app/app.component.ts
+++ frontend/projettraveleef/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './layout/header/header.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import {PageAccueilComponent} from './layout/page-accueil/page-accueil.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,PageAccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
