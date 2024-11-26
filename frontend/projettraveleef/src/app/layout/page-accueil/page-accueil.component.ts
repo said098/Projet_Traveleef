9af817626df7236272f7a98448db2eacd7e350cc
saidkamal <saidkamalshinwari098@gmail.com>
2024-11-26 20:33:42 +0100

    Ajout de SearchComponent dans les imports de page-accueil.component.ts.

--- frontend/projettraveleef/src/app/layout/page-accueil/page-accueil.component.ts
+++ frontend/projettraveleef/src/app/layout/page-accueil/page-accueil.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {FilterComponent} from '../../filter/filter.component';
+import {SearchComponent} from "../../search/search.component";
 
 @Component({
   selector: 'app-page-accueil',
   standalone: true,
-  imports: [RouterLink, FormsModule, RouterLink, FilterComponent,],
+    imports: [RouterLink, FormsModule, RouterLink, FilterComponent, SearchComponent,],
   templateUrl: './page-accueil.component.html',
   styleUrl: './page-accueil.component.css'
 })
