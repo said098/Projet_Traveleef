6198a4992031be2407d6fb7fd80be544be2bb037
saidkamal <saidkamalshinwari098@gmail.com>
2024-12-02 20:20:22 +0100

    Mise à jour des imports pour inclure RouterLink et RouterOutlet dans la page d'accueil.

--- frontend/projettraveleef/src/app/layout/page-accueil/page-accueil.component.ts
+++ frontend/projettraveleef/src/app/layout/page-accueil/page-accueil.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import {FilterComponent} from '../../filter/filter.component';
-import {SearchComponent} from "../../search/search.component";
+
 
 @Component({
   selector: 'app-page-accueil',
   standalone: true,
-    imports: [RouterLink, FormsModule, RouterLink, FilterComponent, SearchComponent,],
+  imports: [RouterLink, FormsModule, RouterLink,  RouterOutlet,],
   templateUrl: './page-accueil.component.html',
   styleUrl: './page-accueil.component.css'
 })
