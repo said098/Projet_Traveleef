9bc772019a6b60cc0146520538a31b102f703677
saidkamal <saidkamalshinwari098@gmail.com>
2024-12-09 22:58:37 +0100

    Amélioration de mappeur pour gérer les valeurs manquantes dans utilisateur_mappeur.py

--- backend/projettraveleefback/src/utils/mappers/utilisateur_mappeur.py
+++ backend/projettraveleefback/src/utils/mappers/utilisateur_mappeur.py
@@ -1,4 +1,11 @@
 from src.app.model import Utilisateur
 
-def mappeur (utilisateur_json):
-    return Utilisateur(utilisateur_json['id'], utilisateur_json['email'], utilisateur_json['tel'], utilisateur_json['password']) if utilisateur_json else None
+def mappeur(utilisateur_json):
+    if not utilisateur_json:
+        return None
+    return Utilisateur(
+        utilisateur_json.get('id'),               
+        utilisateur_json.get('email'),            
+        utilisateur_json.get('tel'),              
+        utilisateur_json.get('password')          
+    )
