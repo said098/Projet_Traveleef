cdbd2ecb2ef759af0be77dfa5561f1e68c2e87b6
saidkamal <saidkamalshinwari098@gmail.com>
2024-12-09 22:59:13 +0100

    Gestion de l'état d'authentification et déconnexion dans header.component.ts

--- frontend/projettraveleef/src/app/layout/header/header.component.ts
+++ frontend/projettraveleef/src/app/layout/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {FormsModule} from '@angular/forms';
+import { AuthService } from '../../services/authentification/auth.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports: [RouterLink, FormsModule,  RouterLink,],
+  imports: [RouterLink, FormsModule,CommonModule,  RouterLink],
   standalone: true,
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService) {}
+
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+  logout(): void {
+    this.authService.logout().subscribe();
+  }
 
 }
