51b629e48c6e800c240a6a49f215ce1de62dfdda
saidkamal <saidkamalshinwari098@gmail.com>
2024-12-09 22:59:01 +0100

    Ajout de redirection si l'utilisateur est authentifié dans auth-page.component.ts

--- frontend/projettraveleef/src/app/authentification/auth-page/auth-page.component.ts
+++ frontend/projettraveleef/src/app/authentification/auth-page/auth-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ConnexionComponent } from '../connexion/connexion.component';
 import { InscriptionComponent } from '../inscription/inscription.component';
 import { AuthCarouselService } from '../../services/authentification/auth-carousel.service';
+import { AuthService } from '../../services/authentification/auth.service';
 
 @Component({
   selector: 'app-auth-page',
@@ -16,12 +17,15 @@ import { AuthCarouselService } from '../../services/authentification/auth-carous
 export class AuthPageComponent implements OnInit {
   transform_style!: string;
 
-  constructor(private authCarouselService: AuthCarouselService) { }
+  constructor(private authCarouselService: AuthCarouselService,private authService: AuthService) { }
 
   ngOnInit(): void {
     this.authCarouselService.transform_style$.subscribe((transform_style) => {
       this.transform_style = transform_style;
       console.log(this.transform_style);
     });
-  } 
+    this.authService.redirectIfAuthenticated();
+
+
+  }
 }
