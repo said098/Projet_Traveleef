fe3ca50a99fb96797af945f5b1373a335a9a21ad
saidkamal <saidkamalshinwari098@gmail.com>
2024-12-10 22:54:08 +0100

    Ajout des routes pour afficher et modifier les informations personnelles.

--- frontend/projettraveleef/src/app/app.routes.ts
+++ frontend/projettraveleef/src/app/app.routes.ts
@@ -5,13 +5,18 @@ import {PageProppsComponent} from './layout/page-propps/page-propps.component';
 import { VolDetailsComponent } from './vol-details/vol-details.component';
 
 import { AuthPageComponent } from './authentification/auth-page/auth-page.component';
+import { UserInfoComponent } from './users/user-info/user-info.component';
+import { ModifierUserInfoComponent } from './users/modifier-user-info/modifier-user-info.component';
 
 export const routes: Routes = [
   { path: '', component: PageAccueilComponent},
   { path: 'proprodetraveleff', component: PageProppsComponent},
   { path: 'voyage', component: VoyageComponent },
   { path: 'vol/:id', component: VolDetailsComponent },
-  { path: 'auth', component: AuthPageComponent }
+  { path: 'auth', component: AuthPageComponent },
+  { path: 'infoPers/:id', component: UserInfoComponent},
+  { path: 'infoPers/edit/:id', component: ModifierUserInfoComponent },
+
 ];
 
 
