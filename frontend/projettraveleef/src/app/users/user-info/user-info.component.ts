e06ac62041af82d395f3422da21f24434e60531d
saidkamal <saidkamalshinwari098@gmail.com>
2024-12-10 22:54:57 +0100

    Ajout de la logique pour afficher et rediriger vers l'édition des informations personnelles.

--- frontend/projettraveleef/src/app/users/user-info/user-info.component.ts
+++ frontend/projettraveleef/src/app/users/user-info/user-info.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/authentification/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-info.component.html',
-  styleUrl: './user-info.component.css'
+  styleUrls: ['./user-info.component.css']
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnInit {
+  user: any; // Stocker les informations de l'utilisateur
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getUserInfo();
+  }
+
+  getUserInfo(): void {
+    this.authService.getUserInfo().subscribe({
+      next: (data) => {
+        this.user = data;
+        console.log('Informations utilisateur :', this.user);
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération des informations utilisateur', err);
+      },
+    });
+  }
+
+  editUserInfo(): void {
+    if (this.user && this.user._id) {
+      this.router.navigate([`/infoPers/edit/${this.user._id}`]); // Utilisation correcte de _id
+    } else {
+      console.error('Impossible de rediriger : ID utilisateur manquant');
+    }
+  }
 }
