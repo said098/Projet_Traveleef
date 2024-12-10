e9aa9d0fbb4a208932480ca375b663975ef12581
saidkamal <saidkamalshinwari098@gmail.com>
2024-12-10 22:53:48 +0100

    Ajout des routes pour les informations personnelles (GET et PUT).

--- backend/projettraveleefback/src/app/route.py
+++ backend/projettraveleefback/src/app/route.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 import src.app.controller as controller
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from bson import ObjectId
 
 route_bl = Blueprint('route_bl', __name__)
 
@@ -25,3 +27,29 @@ def inscription():
 @route_bl.route('/logout', methods=['POST'])
 def logout():
     return controller.logout()
+
+
+
+@route_bl.get('/route')
+@jwt_required()
+def recouperUserId():
+    id = get_jwt_identity() 
+    print("id de ", id)
+    return jsonify({'id': id, 'message': f'Bonjour utilisateur avec ID : {id}'}), 200
+
+
+
+
+@route_bl.route('/infoPerso', methods=['GET'])
+@jwt_required()  
+def infoUser():
+    print("dans route infoPerson")
+    return controller.infoUser()
+
+
+
+@route_bl.route('/update', methods=['PUT'])
+@jwt_required()
+def update_user():
+    print("dans route update")
+    return controller.update_user()
